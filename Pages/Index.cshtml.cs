329ef955251a3d8f38376fab8b3418a155d3035d
Maxime SUBLET (Externe) <EXT.maxime.sublet@silae.fr>
2026-03-12 17:09:26 +0100

    Ajout environment variable

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,9 +5,19 @@ namespace AzureQuizLab.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration configuration;
+
+        public string MaintenanceMode { get; set; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            MaintenanceMode = string.Empty;
+        }
+
         public void OnGet()
         {
-
+           MaintenanceMode = this.configuration.GetValue<string>("MaintenanceMode")!;
         }
     }
 }
